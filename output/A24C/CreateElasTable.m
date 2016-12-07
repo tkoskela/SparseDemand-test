f464ded7779ea71fe523b53c6b80f7bde9d60836
Dr Lars NESHEIM <uctpln0@jake.local>
2016-12-07 11:29:57 +0000

    new figures and tables.

---
+++ output/A24C/CreateElasTable.m
@@ -0,0 +1,53 @@
+function CreateElasTable(filename,elas,q_labels)
+
+J = size(elas,1);
+
+diary(filename);
+
+disp('\documentclass[11pt]{article}');
+disp('\usepackage{geometry}');
+disp('\geometry{a4paper}');
+disp('\usepackage{graphicx}');
+disp('\usepackage{amssymb}');
+disp('\usepackage{epstopdf}');
+disp('\DeclareGraphicsRule{.tif}{png}{.png}{`convert #1 `dirname #1`/`basename #1 .tif`.png}');
+disp('');
+disp('\title{Elasticities}');
+disp('\author{Lars Nesheim}');
+disp('\date{}');
+disp('');
+disp('\begin{document}');
+disp('\maketitle');
+disp('');
+disp('\begin{table}[h]');
+disp('\caption{Elasticities}');
+disp('\label{Table: elasticities}');
+disp('\begin{center}');
+
+
+for j1=1:6
+    
+disp('\begin{tabular}{ccccc}');
+disp(['Price & ',q_labels{4*(j1-1)+1},' & ', ...
+                 q_labels{4*(j1-1)+2},' & ', ...
+                 q_labels{4*(j1-1)+3},' & ', ...
+                 q_labels{4*(j1-1)+4},' \\ \hline']);
+for i1=1:J
+  disp(['p$_{',q_labels{i1},'}$ & ', ...
+        num2str(elas(i1,4*(j1-1)+1)),' & ', ...
+        num2str(elas(i1,4*(j1-1)+2)),' & ', ...
+        num2str(elas(i1,4*(j1-1)+3)),' & ', ...
+        num2str(elas(i1,4*(j1-1)+4)), ...
+        ' \\ \hline']);
+end
+disp('\end{tabular}');
+disp('\end{center}');
+disp('\end{table}%');
+disp('');
+disp('');
+disp('');
+
+end % for j1=1:6
+
+disp('\end{document}');  
+diary off;
