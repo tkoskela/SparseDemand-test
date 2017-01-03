79943e624efc27bae2b5abb5d774fdea5c17645f
Lars Nesheim <l.nesheim@ucl.ac.uk>
2017-01-03 15:16:14 +0000

    move matlab files to matlab directory

--- matlab/ImportElasticity.m
+++ matlab/ImportElasticity.m
@@ -84,5 +84,9 @@ for col=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 end
 
 
+%% Replace non-numeric cells with NaN
+R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
+raw(R) = {NaN}; % Replace non-numeric cells
+
 %% Create output variable
 elas = cell2mat(raw);
