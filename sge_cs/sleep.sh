46b63b311bafad225a81133a93b2f653265fbc37
Dr Lars NESHEIM <uctpln0@vic.local>
2017-01-26 10:20:26 +0000

    sleep script file.

---
+++ sge_cs/sleep.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -wd /home/uctpln0/FruitDemand/code/fortran/source
+#$ -o /home/uctpln0/FruitDemand/code/fortran/output/sleep.out
+#$ -N A24B
+#$ -j y
+#$ -R y
+#$ -cwd
+#$ -V
+#$ -t 1-201
+#$ -l tmem=2.4G,h_vmem=2.4G
+#$ -l h_rt=72:0:0
+##,hostname=burns-*
+##$ -pe orte 401
+##  -terse  -R y -t 1-$np -V $res_list 
+
+cd /home/uctpln0/FruitDemand/code/fortran/source
+source /home/uctpln0/GeneralCode/config/SetEnv.sh
+. /share/apps/econ/Modules/modules.sh
+module load gcc/5.2.0 nag/fll6i25dc intel/composer/2015.1.133 openmpi/intel/1.10.0 totalview knitro
+date
+./sleep.exe
+date
