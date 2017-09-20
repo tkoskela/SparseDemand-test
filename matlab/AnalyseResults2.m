b96af7d177b63d73239bbee9fd848116daf09fab
Dr Lars NESHEIM <uctpln0@vic.local>
2017-09-20 09:42:07 +0100

    new matlab files

---
+++ matlab/AnalyseResults2.m
@@ -0,0 +1,5 @@
+%WorkDir = '/home/uctpln0/FruitDemand/code/fortran/output/A24B';
+WorkDir = '/SAN/economics/Nesheim-IO/FruitDemand/output/A24_20170629';
+file1 = fullfile(WorkDir,'IndividualDemand.txt');
+
+IndividualDemand = ImportIndividualDemand(file1);
