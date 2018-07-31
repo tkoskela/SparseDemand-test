78bc7e2ecf9598d08e9a32a046e481ba67b16b86
Dr Lars NESHEIM <uctpln0@vic.local>
2018-07-31 18:28:12 +0100

    old files.

--- sge_cs/sleep.sh
+++ sge_cs/sleep.sh
@@ -2,7 +2,7 @@
 #$ -S /bin/bash
 #$ -wd /home/uctpln0/FruitDemand/code/fortran/source
 #$ -o /home/uctpln0/FruitDemand/code/fortran/output/sleep1.out
-#$ -N A24ES
+#$ -N A27
 #$ -j y
 #$ -R y
 #$ -cwd
@@ -16,9 +16,6 @@
 ##  -terse  -R y -t 1-$np -V $res_list 
 
 cd /home/uctpln0/FruitDemand/code/fortran/source
-source /home/uctpln0/GeneralCode/config/SetEnv.sh
-. /share/apps/econ/Modules/modules.sh
-module load gcc/5.2.0 nag/fll6i25dc intel/composer/2015.1.133 openmpi/intel/1.10.0 totalview knitro
 date
 ./sleep.exe
 date
