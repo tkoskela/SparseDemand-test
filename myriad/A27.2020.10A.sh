9b894ebdb335137b51f27dd0c92d8ff6e71201d1
uctpln0 <uctpln0@login14.myriad.ucl.ac.uk>
2021-03-06 11:56:31 +0000

    update parameter input files

--- myriad/A27.2020.10A.sh
+++ myriad/A27.2020.10A.sh
@@ -3,18 +3,19 @@
 #$ -l h_rt=120:0:0
 #$ -l mem=4G
 #$ -l tmpfs=4G
-#$ -pe mpi 200
+#$ -pe mpi 800
 #$ -t 1
 #$ -N fruit
 #$ -wd /home/uctpln0/FruitDemand/code/fortran/source
 #$ -o /home/uctpln0/Scratch/FruitDemand/output/a27.2020.10.out
 
-source /shared/ucl/apps/bin/defmods
-module load nag/fortran/mark26/intel-2017
+#source /shared/ucl/apps/bin/defmods
+#module load nag/fortran/mark26/intel-2017
 
 cd /home/uctpln0/FruitDemand/code/fortran/source
 date
 export I_MPI_FABRICS=shm,tcp
+env
 mpirun -genv I_MPI_FABRICS=tcp -np $NSLOTS ./SparseDemand_mpi.exe ../inputs/A27.2020.10/A27A.prop
 date
 
