e396fdd53366f61460478969f07bc2f9d8ea880f
uctpln0 <uctpln0@login14.myriad.ucl.ac.uk>
2021-04-02 15:56:59 +0100

    clean up input directory

---
+++ inputs/Test1/matlab/CreateParameters1.m
@@ -0,0 +1,14 @@
+% Create initial parameter values to simulate multiple discrete choice model
+% based on Lewbel and Nesheim (2019)
+%   https://www.cemmap.ac.uk/publication/sparse-demand-systems-corners-and-complements/?highlight=nesheim
+%
+%  J      = number of products
+%  K      = maximum number purchased
+%  OutDir = subdirectory to store parameters 
+J = 5;
+K = 3;
+OutDir = '../rawparms';
+if exist(OutDir,'dir')==0
+  system(['mkdir ',OutDir]);
+end
+CreateParameters2(J,K,OutDir);
